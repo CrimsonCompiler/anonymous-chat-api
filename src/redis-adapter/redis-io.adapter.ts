422bf92b476b91b543480877a836e94cf71fce32
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-30 17:41:53 +0600

    feat: add Redis adapter implementation for improved scalability

---
+++ src/redis-adapter/redis-io.adapter.ts
@@ -0,0 +1,5 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+import { ServerOptions } from 'socket.io';
+
